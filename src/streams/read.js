132161c6ed52b10214ed5b877e6b30a61da66e75
Павел Бондарь <pavel.bondar@megafon.ru>
2024-10-04 20:43:01 +0300

    feat: add streams task result

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const file = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readStream = createReadStream(file, { encoding: 'utf8' });
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log('\nFinish read file');
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading file', err.message);
+    });
 };
 
 await read();
