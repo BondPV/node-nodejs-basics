45d57782418151ad4cee84caa1e9ed3ee4f16466
Павел Бондарь <pavel.bondar@megafon.ru>
2024-10-04 20:43:18 +0300

    feat: add zip task result

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const file = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const archive = path.join(__dirname, 'files', 'archive.gz');
+
+    const gunzip = createGunzip();
+    const readStream = createReadStream(archive);
+    const writeStream = createWriteStream(file);
+
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('Decompress success');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Error', err.message);
+    });
 };
 
 await decompress();
