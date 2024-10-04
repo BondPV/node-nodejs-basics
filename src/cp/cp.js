c057370e84e008f4f6757241196e61f2ca43326f
Павел Бондарь <pavel.bondar@megafon.ru>
2024-10-04 21:55:35 +0300

    feat: add cp and wt task result

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const scriptPath = path.join(__dirname, 'files', 'script.js');
+
+    const child = spawn('node', [scriptPath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.on('data', (data) => {
+        process.stdout.write(data);
+    });
+
+    child.on('exit', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
